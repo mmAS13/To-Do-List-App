915ae656943ae66db5bdbcbf850a425c1ad4c5fe
mmAS13 <mariummemon003@gmail.com>
2025-12-15 17:25:00 +0500

    Add files via upload

---
+++ webpage/script.js
@@ -0,0 +1,18 @@
+function addTask(){
+const input = document.getElementById("taskInput");
+const taskText = input.value.trim();
+
+
+if(taskText === ""){
+alert("Please enter a task");
+return;
+}
+
+
+const li = document.createElement("li");
+li.innerHTML = `${taskText} <span onclick="this.parentElement.remove()">✖</span>`;
+
+
+document.getElementById("taskList").appendChild(li);
+input.value = "";
+}
